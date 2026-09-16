52bc463a115c0e77d64ce34b18239444adf5d0af
nganht614 <hoangnganbn2000@gmail.com>
2026-09-16 23:07:41 +0700

    Add ex lesson-02

---
+++ tests/lesson-02/ex1.js
@@ -0,0 +1,8 @@
+const number = 12;
+let name = "myNumber";
+let isEven = false;
+
+
+console.log (number);
+console.log (name);
+console.log (isEven);
---
+++ tests/lesson-02/ex2.js
@@ -0,0 +1,9 @@
+// Đề bài: 
+// const myName = "Alex";
+// myName = "Nagi";
+// console.log(myName);
+// Đang thay đổi giá trị của hằng số cho nên báo lỗi 
+//Sửa:
+let myName = "Alex";
+myName = "Nagi";
+console.log(myName);
